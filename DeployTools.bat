2b3b37c25176a4f245fd30670cfd1e69dd0558f0
actenniskrazy <actenniskrazy@f1977f58-96b6-4784-fcef-4aab250471bb>
2012-03-01 07:28:54 +0000

    - Fixed DeployTools files bug; now working

--- DeployTools.bat
+++ DeployTools.bat
@@ -1,13 +1,23 @@
 @Echo off
 cls
+echo.
+echo.
+echo Preparing to deploy...
 
 :: Destination folder
-SET DESTINATION_PATH=%USERPROFILE%\Halliburton RTA Manager\Include
+SET DESTINATION_PATH=%USERPROFILE%\My Documents\Halliburton RTA Manager\Include
 
 :: File paths to copy
-SET RTA_TOOLS=%CD%\..\Include\RTA Sheet Tools.exe
-SET CMD_LIN=%CD%\..\Include\CMDline_Functions.exe
+SET RTA_TOOLS=%CD%\Include\RTA Sheet Tools.exe
+SET CMD_LINE=%CD%\Include\CMDline_Functions.exe
 
+echo Copying file....
+echo.
 
 COPY /Y "%RTA_TOOLS%" "%DESTINATION_PATH%"
+
 COPY /Y "%CMD_LINE%" "%DESTINATION_PATH%"
+
+echo.
+echo.
+echo        Done!
